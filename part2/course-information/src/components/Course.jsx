5a6dc5765a529ddc3fc8f8ec3fd80c4523fe7255
tristan-kkc <13tcheah@gmail.com>
2026-08-18 15:50:17 +0100

    C Complete Exercise 2.2/3 of Course Information

--- part2/course-information/src/components/Course.jsx
+++ part2/course-information/src/components/Course.jsx
@@ -14,11 +14,19 @@ const Part = ({part}) => (
   </p>
 )
 
+const Total = ({parts}) => {
+    const total = parts.reduce((acc, next) => acc += next.exercises, 0)
+    return (
+      <p>total of {total} exercises</p>
+    )
+}
+
 const Course = ({ course }) => {
   return (
     <div>
         <Header text={course.name}/>
         <Content parts={course.parts} />
+        <Total parts={course.parts} />
     </div>
   )
 }
